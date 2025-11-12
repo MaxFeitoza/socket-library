f2cbcde865ad816796d64325ae24b433a1bf6706
Maximus Feitoza Lira Cunha <151573378+MaxFeitoza@users.noreply.github.com>
2025-11-12 14:54:39 -0300

    comentarios

--- cliente.py
+++ cliente.py
@@ -6,9 +6,9 @@ HOST = '127.0.0.1'
 PORT = 60001
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
+s.connect((HOST, PORT)) #conecta com servidor
 
-s.sendall(b'Hello, servidor!')
-data = s.recv(1024)
-print(f"Recebido: {data.decode()}")
-s.close()
+s.sendall(b'Hello, servidor!') #manda mensagem para o servidor
+data = s.recv(1024) #recebe resposta do servidor
+print(f"Recebido: {data.decode()}")#mostra a resposta
+s.close() #fecha a conexao

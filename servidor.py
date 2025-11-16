450900dce1f8373b1e9f2cb2311f58a58d1cf2d5
Maximus Feitoza Lira Cunha <151573378+MaxFeitoza@users.noreply.github.com>
2025-11-16 17:19:58 -0300

    Refactor: Corrigindo codigo de acordos com as exigencias do professor

--- servidor.py
+++ servidor.py
@@ -1,33 +1,47 @@
 import socket 
 import sys
 
-HOST = 'localhost'
-PORT = 60001
+HOST = '127.0.0.1'
+PORT = 6789
 BUFFER_SIZE = 1024
 
 #af_inet = ENDERECO IPV4
 #SOCK_STREAM = TCP
 #SOCKET_DGRAM = UDP 
 # Create a TCP/IP socket 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serverSocket  = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #cria o socket
 #VINCULA A PORTA COM ENDERECO IP
-s.bind((HOST, PORT))
+serverSocket.bind((HOST, PORT))
 
 #MODO ESCUTA
-s.listen(1)
-print (f"Aguardando conexao na porta {PORT}")
-#ACEITA A CONEXAO
-conn, addr = s.accept()
-print(f"Conectado por {addr}")
+serverSocket.listen(1) #tamanho maximo de conexoes ( outros ficam na fila de espera )
 
 #LOOP DE RECEPCAO DE DADOS
 while True:
-    #recebe DADOS com BUFFER_SIZE ( tamnho maximo de bytes  )
-    data = conn.recv(BUFFER_SIZE)
-    if not data:
-        print("Conexao encerrada pelo cliente")
-        conn.close()
-        break
-    
-    print(f"Recebido: {data.decode()}")
-    conn.sendall(data)  #ECOA OS DADOS RECEBIDOS
+    print("Ready to serve...")
+    connectionSocket, addr = serverSocket.accept() #aceita a conexao do cliente
+    try:
+        message = connectionSocket.recv(BUFFER_SIZE).decode() #recebe mensagem do cliente com BUFFER_SIZE ( tamnho maximo de bytes  )
+        filename = message.split()[1] 
+        f = open(filename[1:]) 
+        outputdata = f.readlines() #le o arquivo e retorna uma lista de linhas
+        f.close()  # Fecha o arquivo
+        
+        connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n".encode())
+        
+        for i in range(0, len(outputdata)): 
+            connectionSocket.send(outputdata[i].encode()) #envia os dados para o cliente "send"
+        connectionSocket.send("\r\n".encode())
+        
+        # Fecha a conexão com o cliente após enviar o arquivo
+        connectionSocket.close()
+    except IOError:
+        # Envia mensagem de erro 404 se o arquivo não for encontrado
+        connectionSocket.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
+        # Fecha o socket do cliente
+        connectionSocket.close()
+        break # Nao e necessario continuar o loop se o arquivo nao for encontrado 
+
+serverSocket.close()
+sys.exit() #encerra o programa
+
